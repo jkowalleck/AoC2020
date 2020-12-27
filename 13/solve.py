861b5abbd55b58460cf1036ab5ed0b9ddaf7d770
Jan Kowalleck <jan.kowalleck@gmail.com>
2020-12-27 18:09:35 +0100

    13 b

--- 13/solve.py
+++ 13/solve.py
@@ -1,3 +1,4 @@
+from collections import namedtuple
 from os import path
 from typing import List
 
@@ -9,23 +10,38 @@ def get_input() -> List[str]:
         return fh.read().splitlines()
 
 
-def solve() -> int:
-    input = get_input()
-    arrival = int(input[0])
-    busses = set(map(int, filter(lambda b: b != 'x', input[1].split(','))))
-    departures = dict()
-    for bus in busses:
-        last = arrival % bus
-        wait = bus - last
-        departures[wait] = bus
-        pass
+def solve1() -> int:
+    lines = get_input()
+    arrival = int(lines[0])
+    busses = set(map(int, filter(lambda b: b != 'x', lines[1].split(','))))
+    departures = {bus - (arrival % bus): bus for bus in busses}
     min_wait = min(departures.keys())
     bus_to_take = departures[min_wait]
     return min_wait * bus_to_take
 
 
+def solve2() -> int:
+    lines = get_input()
+    _Bus = namedtuple('_Bus', ['number', 'pos'])
+    busses = [_Bus(int(bus), pos) for pos, bus in enumerate(lines[1].split(',')) if bus != 'x']
+
+    def next_possible_time(time_: int, again_: int, bus: _Bus) -> int:
+        while (time_ + bus.pos) % bus.number != 0:
+            time_ += again_
+        return time_
+
+    again = 1
+    time = 0
+    for bus in busses:
+        time = next_possible_time(time, again, bus)
+        if all(0 == ((time + bus.pos) % bus.number) for bus in busses):
+            return time
+        again *= bus.number
+    return 0
+
+
 if __name__ == '__main__':
-    solution_part1 = solve()
+    solution_part1 = solve1()
     print(f'solution part 1: {solution_part1}')
-    #solution_part2 = solve()
-    #print(f'solution part 2: {solution_part2}')
+    solution_part2 = solve2()
+    print(f'solution part 2: {solution_part2}')
